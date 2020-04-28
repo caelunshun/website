4c4d42bd924a74a060ef756c53f84d60d5379ee8
Jacob Emil Ulvedal Rosborg <jacob@rosborg.dk>
2020-04-28 10:16:39 +0200

    cssnano, header, metadesc, and changed the title a bit.

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -5,5 +5,6 @@ module.exports = {
         },
         "posthtml-md": {},
         "posthtml-img-autosize": {},
+        "posthtml-inline-assets": {}
     }
 };
--- postcss.config.js
+++ postcss.config.js
@@ -38,6 +38,5 @@ module.exports = {
       },
       foundries: ['google']
     },
-    // '@fullhuman/postcss-purgecss': {}
   }
 }
