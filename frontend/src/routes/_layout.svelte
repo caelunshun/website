f8de6fd08380e8fb6e2713a6d8668b55258ed800
FussballAndy <andreas.brodner@t-online.de>
2021-04-01 10:30:22 +0200

    Preparinng dropdown

--- frontend/src/routes/_layout.svelte
+++ frontend/src/routes/_layout.svelte
@@ -4,8 +4,10 @@
 	import GithubMark from "$assets/github-mark.svg"
     import { LockIcon, SettingsIcon } from "svelte-feather-icons"
     import { onMount } from "svelte";
+    import DropDownSvg from "../components/DropDownSVG.svelte";
 
     let isDropdownShown = false;
+    let isAccountDropdownShown = false;
 
 </script>
 
@@ -22,16 +24,27 @@
             <!-- <li><a href="https://github.com/feather-rs/feather"><GithubMark class="h-8 sm:h-12 fill-current text-feather-light hover:text-white" /></a></li> -->
             <!-- Should be logged in users avatar -->
             {#if !$token.secret}
-            <li><a href="/me"><SettingsIcon class="h-6 w-6" /><span class="mx-2 my-auto">Account</span></a></li>
-            <hr class="md:hidden w-4/5 mx-auto">
+            <li noflex class="flex md:hidden"><a href="/me">
+                <SettingsIcon class="h-6 w-6" />
+                <span class="mx-2 my-auto">Account</span>
+            </a></li>
             <li noflex class="font-normal flex md:hidden"><a href="/me"><LockIcon class="h-6 w-6" /><span class="mx-2 my-auto">Logout</span></a></li>
+            <li noflex class="hidden md:flex cursor-pointer hover:underline relative" on:focus={() => isAccountDropdownShown = true} on:blur={() => isAccountDropdownShown = false}>
+                <span class="mx-2 my-auto">Account</span>
+                <div class="my-auto">
+                    <DropDownSvg color="ffffff" width={20} height={20} />
+                </div>
+                {#if isAccountDropdownShown}
+                    <div></div>
+                {/if}
+            </li>
             {:else}
             <!-- How should the login button look? -->
             <li><a href="/me/login" class="flex items-center"><LockIcon class="h-6 w-6" /><span class="mx-2 my-auto">Login (GitHub)</span></a></li>
             {/if}    
         </ul>
         <div class="absolute right-6 top-9 md:hidden z-50 text-white transform -translate-x-1/2 -translate-y-1/2 {isDropdownShown ? "" : "rotate-90"}" on:click={() => {isDropdownShown = !isDropdownShown;}}>
-            <svg height='25px' width='25px'  fill="#ffffff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><g><path d="M47,73.7L6.5,33.2C4.8,31.5,4.5,28.8,6,27c1.7-2.1,4.8-2.2,6.7-0.3L47,61c1.6,1.6,4.3,1.6,5.9,0l34.3-34.3   c1.8-1.8,4.6-1.8,6.4,0c1.8,1.8,1.8,4.6,0,6.4L53,73.7C51.3,75.4,48.7,75.4,47,73.7z"></path></g></svg>
+            <DropDownSvg color="ffffff" width={25} height={25} />
         </div>
     </nav>
 </header>
@@ -39,7 +52,10 @@
 
 <style>
     li > a {
-        @apply flex tracking-wide md:tracking-normal mx-auto;
+        @apply tracking-wide md:tracking-normal mx-auto hover:underline;
+    }
+    li > a:not([noflex]) {
+        @apply flex;
     }
     li:not([noflex]) {
         @apply flex;
