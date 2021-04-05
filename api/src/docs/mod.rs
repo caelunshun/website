134b93cf0c52ab8d76f5d7b89820096b35f51ea3
FussballAndy <andreas.brodner@t-online.de>
2021-04-05 23:34:25 +0200

    Syntax highlighting

--- api/src/docs/mod.rs
+++ api/src/docs/mod.rs
@@ -5,9 +5,24 @@ pub use summary::*;
 pub use summary_parser::*;
 
 use anyhow::Result;
-use pulldown_cmark::{html, CowStr, Event, Parser, Tag};
-use std::{iter, str::FromStr};
+use pulldown_cmark::{html, CowStr, Event, Parser, Tag, CodeBlockKind};
+use std::{iter, str::FromStr, path::Path};
 use url::Url;
+
+use syntect::{highlighting::{Theme, ThemeSet}, html::highlighted_html_for_string, parsing::{SyntaxSet, SyntaxSetBuilder}};
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref SYNTAXSET: SyntaxSet = {
+        let mut builder: SyntaxSetBuilder = SyntaxSet::load_defaults_newlines().into_builder();
+        builder.add_from_folder(Path::new("./syntaxes/TOML"), true).expect("Failed to load TOML Syntax. Is it up-to-date?");
+        builder.build()
+    };
+    pub static ref THEMESET: ThemeSet = ThemeSet::load_defaults();
+    pub static ref THEME: Theme = THEMESET.themes["base16-ocean.dark"].clone();
+
+}
 
 pub struct DocsParser<'a> {
     base: Url,
@@ -23,6 +38,12 @@ impl<'a> DocsParser<'a> {
     }
 
     pub fn parse(mut self) -> String {
+        let mut syntax = SYNTAXSET.find_syntax_by_extension("rs").unwrap();
+        
+        let mut new_p = Vec::new();
+        let mut to_highlight = String::new();
+        let mut in_code_block = false;
+
         let mut output = String::new();
         for mut event in self.events {
             match &mut event {
@@ -34,12 +55,43 @@ impl<'a> DocsParser<'a> {
                         },
                         Ok(url) => CowStr::from(url.to_string()),
                         _ => CowStr::from("foo"),
+                    };
+                    new_p.push(event);
+                }
+                Event::Start(Tag::CodeBlock(cb)) => {
+                    match cb {
+                        CodeBlockKind::Fenced(token) => {
+                            in_code_block = true;
+                            if let Some(syn) = SYNTAXSET.find_syntax_by_token(token) {
+                                syntax = syn;
+                            } else {
+                                syntax = SYNTAXSET.find_syntax_by_extension("rs").unwrap();
+                            }
+                        }
+                        _ => {}
                     }
                 }
-                _ => {}
+                Event::End(Tag::CodeBlock(_)) => {
+                    if in_code_block {
+                        let html = highlighted_html_for_string(&to_highlight, &SYNTAXSET, &syntax, &THEME);
+                        new_p.push(Event::Html(CowStr::from(html)));
+                        to_highlight = String::new();
+                        in_code_block = false;
+                    }
+                }
+                Event::Text(t) => {
+                    if in_code_block {
+                        to_highlight.push_str(&t);
+                    } else {
+                        new_p.push(event);
+                    }
+                }
+                e => {
+                    new_p.push(e.clone());
+                }
             }
-            html::push_html(&mut output, iter::once(event));
         }
+        html::push_html(&mut output, new_p.into_iter());
         output
     }
 }
